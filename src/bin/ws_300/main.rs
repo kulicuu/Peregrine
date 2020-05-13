2439983c740d4ad930208e9345d4501c487417b4
Wylie Кулик <kulik.wylie@protonmail.com>
2020-05-13 11:49:38 -0500

    update to winit 0.22.1

--- src/bin/ws_300/main.rs
+++ src/bin/ws_300/main.rs
@@ -2,18 +2,17 @@
 
 use peregrine::ash_utility;
 use peregrine::ash_utility::constants::*;
+use peregrine::ash_utility::debug::ValidationInfo;
 use peregrine::ash_utility::share;
 
 use ash::version::EntryV1_0;
 use ash::version::InstanceV1_0;
 // use ash::{vk_version_major, vk_version_minor, vk_version_patch};
 use ash::vk::{version_major, version_minor, version_patch};
-
-
 use ash::vk;
 
-
 use std::ffi::CString;
+use std::os::raw::c_char;
 use std::ptr;
 
 use winit::event::{Event, VirtualKeyCode, ElementState, KeyboardInput, WindowEvent};
@@ -48,13 +47,12 @@ struct VulkanApp {
     debug_utils_loader: ash::extensions::ext::DebugUtils,
     debug_messager: vk::DebugUtilsMessengerEXT,
     _physical_device: vk::PhysicalDevice,
+    device: ash::Device,
+    _graphics_queue: vk::Queue,
 }
 
-
 impl VulkanApp {
-
     pub fn new() -> VulkanApp {
-
         let entry = ash::Entry::new().unwrap();
         let instance = share::create_instance(
             &entry,
@@ -66,6 +64,8 @@ impl VulkanApp {
 
         let (debug_utils_loader, debug_messager) = ash_utility::debug::setup_debug_utils(VALIDATION.is_enable, &entry, &instance);
         let physical_device = VulkanApp::pick_physical_device(&instance);
+        let (logical_device, graphics_queue) =
+            VulkanApp::create_logical_device(&instance, physical_device, &VALIDATION);
 
         VulkanApp {
             _entry: entry,
@@ -73,6 +73,8 @@ impl VulkanApp {
             debug_utils_loader,
             debug_messager,
             _physical_device: physical_device,
+            device: logical_device,
+            _graphics_queue: graphics_queue,
         }
     }
 
@@ -83,21 +85,25 @@ impl VulkanApp {
                 .expect("Failed to enumerate Physical Devices.")
         };
 
-        println!("{} devices (GPU) found with Vulkan support.", physical_devices.len());
+        // println!("{} devices (GPU) found with Vulkan support.", physical_devices.len());
 
-        let mut result = None;
+        // let mut result = None;
 
-        for &physical_device in physical_devices.iter() {
-            if VulkanApp::is_physical_device_suitable(instance, physical_device) {
-                if result.is_none() {
-                    result = Some(physical_device)
-                }
-            }
-        }
+        let result = physical_devices.iter().find(|physical_device| {
+            VulkanApp::is_physical_device_suitable(instance, **physical_device)
+        });
+
+        // for &physical_device in physical_devices.iter() {
+        //     if VulkanApp::is_physical_device_suitable(instance, physical_device) {
+        //         if result.is_none() {
+        //             result = Some(physical_device)
+        //         }
+        //     }
+        // }
 
         match result {
             None => panic!("Failed to find a suitable GPU."),
-            Some(physical_device) => physical_device,
+            Some(p_physical_device) => *p_physical_device,
         }
     }
 
@@ -107,79 +113,146 @@ impl VulkanApp {
     ) -> bool {
         let device_properties = unsafe { instance.get_physical_device_properties(physical_device)  };
         let device_features = unsafe { instance.get_physical_device_features(physical_device) };
-        let device_queue_families = unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
-
-        let device_type = match device_properties.device_type {
-            vk::PhysicalDeviceType::CPU => "Cpu",
-            vk::PhysicalDeviceType::INTEGRATED_GPU => "Integrated GPU",
-            vk::PhysicalDeviceType::DISCRETE_GPU => "Discrete GPU",
-            vk::PhysicalDeviceType::OTHER => "Unknown",
-            _ => panic!(),
-        };
-
-        let device_name = ash_utility::tools::vk_to_string(&device_properties.device_name);
-        println!(
-            "\tDevice Name: {}, id: {}, type: {}",
-            device_name, device_properties.device_id, device_type
-        );
-
-        let major_version = version_major(device_properties.api_version);
-        let minor_version = version_minor(device_properties.api_version);
-        let patch_version = version_patch(device_properties.api_version);
-
-        println!(
-            "\tAPI Version: {}.{}.{}",
-            major_version, minor_version, patch_version
-        );
-
-        println!("\tSupport Queue Family: {}", device_queue_families.len());
-        println!("\t\tQueue Count | Graphics, Compute, Transfer, Sparse Binding");
-
-        for queue_family in device_queue_families.iter() {
-            let is_graphics_support = if queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
-                "support"
-            } else {
-                "unsupport"
-            };
-            let is_compute_support = if queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE) {
-                "support"
-            } else {
-                "unsupported"
-            };
-            let is_transfer_support = if queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER) {
-                "supported"
-            } else {
-                "unsupported"
-            };
-            let is_sparse_support = if queue_family.queue_flags.contains(vk::QueueFlags::SPARSE_BINDING) {
-                "supported"
-            } else {
-                "unsupported"
-            };
-
-            println!(
-                "\t\t{}\t     | {}, {}, {}, {}",
-                queue_family.queue_count,
-                is_graphics_support,
-                is_compute_support,
-                is_transfer_support,
-                is_sparse_support
-            );
-        }
-
-        println!(
-            "\tGeometry Shader support: {}",
-            if device_features.geometry_shader == 1 {
-                "Supported"
-            } else {
-                "Unsupported"
-            }
-        );
+        // let device_queue_families = unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
+        //
+        // let device_type = match device_properties.device_type {
+        //     vk::PhysicalDeviceType::CPU => "Cpu",
+        //     vk::PhysicalDeviceType::INTEGRATED_GPU => "Integrated GPU",
+        //     vk::PhysicalDeviceType::DISCRETE_GPU => "Discrete GPU",
+        //     vk::PhysicalDeviceType::OTHER => "Unknown",
+        //     _ => panic!(),
+        // };
+        //
+        // let device_name = ash_utility::tools::vk_to_string(&device_properties.device_name);
+        // println!(
+        //     "\tDevice Name: {}, id: {}, type: {}",
+        //     device_name, device_properties.device_id, device_type
+        // );
+        //
+        // let major_version = version_major(device_properties.api_version);
+        // let minor_version = version_minor(device_properties.api_version);
+        // let patch_version = version_patch(device_properties.api_version);
+        //
+        // println!(
+        //     "\tAPI Version: {}.{}.{}",
+        //     major_version, minor_version, patch_version
+        // );
+        //
+        // println!("\tSupport Queue Family: {}", device_queue_families.len());
+        // println!("\t\tQueue Count | Graphics, Compute, Transfer, Sparse Binding");
+        //
+        // for queue_family in device_queue_families.iter() {
+        //     let is_graphics_support = if queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
+        //         "support"
+        //     } else {
+        //         "unsupport"
+        //     };
+        //     let is_compute_support = if queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE) {
+        //         "support"
+        //     } else {
+        //         "unsupported"
+        //     };
+        //     let is_transfer_support = if queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER) {
+        //         "supported"
+        //     } else {
+        //         "unsupported"
+        //     };
+        //     let is_sparse_support = if queue_family.queue_flags.contains(vk::QueueFlags::SPARSE_BINDING) {
+        //         "supported"
+        //     } else {
+        //         "unsupported"
+        //     };
+        //
+        //     println!(
+        //         "\t\t{}\t     | {}, {}, {}, {}",
+        //         queue_family.queue_count,
+        //         is_graphics_support,
+        //         is_compute_support,
+        //         is_transfer_support,
+        //         is_sparse_support
+        //     );
+        // }
+        //
+        // println!(
+        //     "\tGeometry Shader support: {}",
+        //     if device_features.geometry_shader == 1 {
+        //         "Supported"
+        //     } else {
+        //         "Unsupported"
+        //     }
+        // );
 
         let indices = VulkanApp::find_queue_family(instance, physical_device);
 
         return indices.is_complete();
     }
+
+    fn create_logical_device(
+        instance: &ash::Instance,
+        physical_device: vk::PhysicalDevice,
+        validation: &ValidationInfo,
+    ) -> (ash::Device, vk::Queue) {
+        let indices = VulkanApp::find_queue_family(instance, physical_device);
+        let queue_priorities = [1.0_f32];
+        let queue_create_info = vk::DeviceQueueCreateInfo {
+            s_type: vk::StructureType::DEVICE_QUEUE_CREATE_INFO,
+            p_next: ptr::null(),
+            flags: vk::DeviceQueueCreateFlags::empty(),
+            queue_family_index: indices.graphics_family.unwrap(),
+            p_queue_priorities: queue_priorities.as_ptr(),
+            queue_count: queue_priorities.len() as u32,
+        };
+
+        let physical_device_features = vk::PhysicalDeviceFeatures {
+            ..Default::default()
+        };
+
+        let required_validation_layer_raw_names: Vec<CString> = validation
+            .required_validation_layers
+            .iter()
+            .map(|layer_name| CString::new(*layer_name).unwrap())
+            .collect();
+        let enable_layer_names: Vec<*const c_char> = required_validation_layer_raw_names
+            .iter()
+            .map(|layer_name| layer_name.as_ptr())
+            .collect();
+
+        let device_create_info = vk::DeviceCreateInfo {
+            s_type: vk::StructureType::DEVICE_CREATE_INFO,
+            p_next: ptr::null(),
+            flags: vk::DeviceCreateFlags::empty(),
+            queue_create_info_count: 1,
+            p_queue_create_infos: &queue_create_info,
+            enabled_layer_count: if validation.is_enable {
+                enable_layer_names.len()
+            } else {
+                0
+            } as u32,
+            pp_enabled_layer_names: if validation.is_enable {
+                enable_layer_names.as_ptr()
+            } else {
+                ptr::null()
+            },
+            enabled_extension_count: 0,
+            pp_enabled_extension_names: ptr::null(),
+            p_enabled_features: &physical_device_features,
+        };
+
+        let device: ash::Device = unsafe {
+            instance
+                .create_device(physical_device, &device_create_info, None)
+                .expect("Failed to create logical Device.")
+        };
+
+
+        // println!("device: {:?}", device);
+        // device = 44;
+        // let y: i32 = device;
+        let graphics_queue = unsafe { device.get_device_queue(indices.graphics_family.unwrap(), 0) };
+
+        (device, graphics_queue)
+    }
+
 
     fn find_queue_family(
         instance: &ash::Instance,
--- src/bin/ws_400/main.rs
+++ src/bin/ws_400/main.rs
@@ -1,5 +1,128 @@
 
 
+// was studying vulkan-tutorial-rust and got hit with breaking changes as this version of Ash 0.31.0 has breaking changes against the tutorial's usage of legacy ash 0.29.0.  So going through the Ash example which is being updated recently to guage their recommended usage -- also check the issue critiquing the patterns in the examples.  Will go through this a few times and maybe take revisions back to a fork of the tutorial code.
+
+
+use winapi;
+
+
+use ash::extensions::khr::Win32Surface;
+pub use ash::version::{DeviceV1_0, EntryV1_0, InstanceV1_0};
+use ash::{vk, Device, Entry, Instance};
+use std::borrow::Cow;
+use std::cell::RefCell;
+use std::default::Default;
+use std::ffi::{CStr, CString};
+use std::ops::Drop;
+use std::os::raw::c_void;
+// use winit::window::Window;
+
+
+#[macro_export]
+macro_rules! offset_of {
+    ($base:path, $field:ident) => {{
+        #[allow(unused_unsafe)]
+        unsafe {
+            let b: $base = mem::zeroed();
+            (&b.$field as *const _ as isize) - (&b as *const _ as isize)
+        }
+    }};
+}
+
+
+pub fn record_submit_commandbuffer<D: DeviceV1_0, F: FnOnce(&D, vk::CommandBuffer)>(
+    device: &D,
+    command_buffer: vk::CommandBuffer,
+    submit_queue: vk::Queue,
+    wait_mask: &[vk::PipelineStageFlags],
+    wait_semaphores: &[vk::Semaphore],
+    signal_semaphores: &[vk::Semaphore],
+    f: F,
+) {
+    unsafe {
+        device
+            .reset_command_buffer(
+                command_buffer,
+                vk::CommandBufferResetFlags::RELEASE_RESOURCES,
+            )
+            .expect("Reset command buffer failed.");
+
+        let command_buffer_begin_info = vk::CommandBufferBeginInfo::builder()
+            .flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT);
+
+        device
+            .begin_command_buffer(command_buffer, &command_buffer_begin_info)
+            .expect("Begin commandbuffer");
+
+        f(device, command_buffer);
+
+        device
+            .end_command_buffer(command_buffer)
+            .expect("End commandbuffer");
+
+        let submit_fence = device
+            .create_fence(&vk::FenceCreateInfo::default(), None)
+            .expect("Create fence failed.");
+
+        let command_buffers = vec![command_buffer];
+
+        let submit_info = vk::SubmitInfo::builder()
+            .wait_semaphores(wait_semaphores)
+            .wait_dst_stage_mask(wait_mask)
+            .command_buffers(&command_buffers)
+            .signal_semaphores(signal_semaphores);
+
+        device
+            .queue_submit(submit_queue, &[submit_info.build()], submit_fence)
+            .expect("queue submit failed");
+
+        device
+            .wait_for_fences(&[submit_fence], true, std::u64::MAX)
+            .expect("Wait for fence failed");
+
+        device.destroy_fence(submit_fence, None);
+    }
+}
+
+#[cfg(target_os = "windows")]
+unsafe fn create_surface<E: EntryV1_0, I: InstanceV1_0>(
+    entry: &E,
+    instance: &I,
+    window: &winit::window::Window,
+) -> Result<vk::SurfaceKHR, vk::Result> {
+    use std::ptr;
+    use winapi::shared::windef::HWND;
+    use winapi::um::libloaderapi::GetModuleHandleW;
+    use winit::platform::windows::WindowExtWindows;
+    let hwnd = window.hwnd() as HWND;
+    let hinstance = GetModuleHandleW(ptr::null()) as *const c_void;
+    let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
+        s_type: vk::StructureType::WIN32_SURFACE_CREATE_INFO_KHR,
+        p_next: ptr::null(),
+        flags: Default::default(),
+        hinstance,
+        hwnd: hwnd as *const c_void,
+    };
+    let win32_surface_loader = Win32Surface::new(entry, instance);
+    win32_surface_loader.create_win32_surface(&win32_create_info, None)
+}
+
+
+
+
+pub struct AshBase {
+    pub entry: Entry,
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -7,12 +130,6 @@
 fn main () {
     println!("ws-400");
 
-    unsafe {
-        let mut x = 5;
-        let raw = &mut x as *mut i32;
-        println!("raw: {:?}", *raw);
-        *raw = 10;
-        println!("now raw {:?}", *raw);
-    }
+
 
 }
